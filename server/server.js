87b81755ea8929d3782229316177c2033b27dd95
rohit kumawat <108802657+Hitman1867@users.noreply.github.com>
2025-09-07 11:28:44 +0530

    Remove Donate.css and Donate.js

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ const AdoptFormRoute = require('./Routes/AdoptFormRoute')
 const AdminRoute = require('./Routes/AdminRoute')
 const cors = require('cors');
 const path = require('path');
-const paypal = require('@paypal/checkout-server-sdk');
+ 
 const app = express();
-const PaymentRoute = require('./Routes/PaymentRoute');
+ 
 app.use(cors());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(petRouter)
 app.use('/form', AdoptFormRoute)
 app.use('/admin', AdminRoute)
-app.use('/payment', PaymentRoute);
+ 
 mongoose.connect(process.env.mongooseURL)
     .then(() => {
         console.log('Connected to DB');
